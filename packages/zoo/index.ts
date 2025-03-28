28ec1d4bbb8a90df44e9b6d8c8cc6b7395d14fcc
Leeco <leeco19@qq.com>
2025-03-28 11:56:21 +0800

    chore: update

--- packages/zoo/index.ts
+++ packages/zoo/index.ts
@@ -4,4 +4,12 @@ import { getRandomName } from "@tuskdesign/names";
 const name = getRandomName();
 const animal = getRandomAnimal();
 
+// 请写一段无用代码
+export const uselessCode = () => {
+  const a = 1;
+  const b = 2;
+  const c = a + b;
+  return c;
+};
+
 console.log(`${name} the ${animal.name} says ${animal.sound}!`);
